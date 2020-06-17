5083b37212d5305eb838d7208acecb536deadc7e
ctyFL <502651020@qq.com>
2020-06-17 14:58:00 +0800

    初始化springboot项目

---
+++ pump_learning/pump-01/src/test/java/com/flbrotherhood/pump/PumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flbrotherhood.pump;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
